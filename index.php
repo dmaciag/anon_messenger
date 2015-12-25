2cd9f281cb105e8a89861bc1ab786beb78fe6224
Daniel Maciag <daniel@vettery.com>
2015-12-24 21:41:31 -0500

    added login page

--- index.php
+++ index.php
@@ -1,6 +1,37 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
 
-
-echo "testing";
-
-?>
+    <title>Sign in</title>
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <!--<link href="signin.css" rel="stylesheet">-->
+    <link type="text/css" href="./css/styles.css" rel="stylesheet">
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+  </head>
+  <body>
+    <div class="container">
+      <form class="form-signin" style="max-width: 300px !important; margin-left: 50%; margin-right: 50%; margin:auto;">
+        <h2 class="form-signin-heading" >Please sign in</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" class="form-control"  placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox" >
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me?
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" style="max-width: 148px; display: inline-block;">Sign in</button>
+        <button type="button" class="btn btn-default" style="margin-left: 2px !important; padding: 12px 46px !important;">Register</button>
+      </form>
+    </div> 
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
