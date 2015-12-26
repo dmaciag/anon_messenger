ea5985956177351e808b4fc886354015142c9fa2
Daniel Maciag <daniel@vettery.com>
2015-12-25 19:59:37 -0500

    added stuff

--- index.php
+++ index.php
@@ -12,12 +12,12 @@
   </head>
   <body>
     <div class="container">
-      <form class="form-signin" style="max-width: 300px !important; margin-left: 50%; margin-right: 50%; margin:auto;">
+      <form class="form-signin" action="./app/signin.php" method="post" style="max-width: 300px !important; margin-left: 50%; margin-right: 50%; margin:auto;">
         <h2 class="form-signin-heading" >Please sign in</h2>
         <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control"  placeholder="Email address" required autofocus>
+        <input type="email" name="email" id="inputEmail" style="margin-bottom: 4px;" class="form-control"  placeholder="Email address" required autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
         <div class="checkbox" >
           <label>
             <input type="checkbox" value="remember-me"> Remember me?
