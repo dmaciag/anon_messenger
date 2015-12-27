743d56d740be693ea375325eca94e61b1535daf9
Daniel Maciag <daniel@vettery.com>
2015-12-27 10:54:17 -0500

    added logic for sign in authorization

--- app/signin.php
+++ app/signin.php
@@ -10,33 +10,27 @@ $conn = new mysqli($server, $username, $password, $db);
 if($conn->connect_error){
 	die("Connection failed.\n");
 }
-else{
-	echo "Connected successfuly to mysql." . '<br>';
-}
-
-$sql = "SELECT username, password FROM registered_users";
-$registered_users = $conn->query($sql);
-
-if($registered_users->num_rows > 0 ){
-	echo "exists a user " . '<br>';
-	var_dump($registered_users);
-	/*
-	while($user = $registered_users->fetch_assoc()){
-		echo "username: " $user["username"] . ", password: " . $user["password"] . '<br>';
-	}
-	*/
-}
-else{
-	echo "no users found." .'<br>';
-}
 
 $email = $_POST[email];
 $password = $_POST[password];
 
-$conn->close();
+$sql = "SELECT * FROM registered_users WHERE username = \"$email\"";
+$registered_users = $conn->query($sql);
 
-echo $email . '<br>' . $password . '<br>';
-echo "Sign In";
+if($registered_users->num_rows == 1 ){
+	$user = $registered_users->fetch_assoc();
+	if($user["username"] == $email && $user["password"] == $password){
+		echo "Welcome, $email !";
+	}
+	else{
+		echo "Invalid password !";
+	}
+}
+else{
+	echo "User is not registered. " .'<br>';
+}
+
+$conn->close();
 
 
 ?>
