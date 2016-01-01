adb3a9cefe0c84558e331c58961961350ec0b744
Daniel Maciag <daniel@vettery.com>
2016-01-01 16:11:24 -0500

    changed logic to mysql from mysqli

--- app/signin.php
+++ app/signin.php
@@ -5,17 +5,31 @@ $username = "root";
 $password = "root";
 $db = "my_db";
 
-mysql_connect("$hostname", )
+$connect = mysql_connect($hostname, $username, $password);
+
+if( !$connect ) die('Connection to mysql failed, error : ' . mysql_error());
+
+if( !mysql_select_db($db) ) die('Cannot connect to db : $db, ' . mysql_error());
 
 $email = $_POST[email];
+$email = stripslashes($email);
+
 $password = $_POST[password];
+$password = stripslashes($password);
 
-$sql = "SELECT username, password FROM registered_users WHERE username = \"$email\"";
-$registered_users = $conn->query($sql);
+$sql_query = "SELECT username, password FROM registered_users WHERE username = \"$email\"";
+$registered_users = mysql_query($sql_query);
 
-if($registered_users->num_rows == 1 ){
-	$user = $registered_users->fetch_assoc();
-	if($user["username"] == $email && $user["password"] == $password){
+if( !$registered_users ){
+	$error_mesage  = 'Invalid query error: ' . mysql_error() . "\n";
+	$error_mesage .= 'Desired query: ' . $sql_query;
+	die($error_mesage);
+}
+
+if( mysql_num_rows($registered_users) == 1 ){
+	$user = mysql_fetch_assoc($registered_users);
+
+	if( $user["username"] == $email && $user["password"] == $password ){
 		echo "Welcome, $email !";
 	}
 	else{
@@ -26,7 +40,7 @@ else{
 	echo "User is not registered. " .'<br>';
 }
 
-$conn->close();
+mysql_close($connect);
 
 
 ?>
