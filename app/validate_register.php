ddc724f534ff3daf504cbfd640037ad06d36c98c
Daniel Maciag <daniel@vettery.com>
2016-01-04 09:52:41 -0500

    checked registrationg post parameters

--- app/validate_register.php
+++ app/validate_register.php
@@ -1,5 +1,10 @@
 <?php
 
+echo $_POST[nickname] . "<br>";
+echo $_POST[email] . "<br>";
+echo $_POST[password_first] . "<br>";
+echo $_POST[password_second] . "<br>";
+
 echo "successfuly Registered.\n";
 
 ?>
