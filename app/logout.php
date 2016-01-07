7c07fb741e74247648ae5094a9e4f0ae00b0dab7
Daniel Maciag <daniel@vettery.com>
2016-01-07 00:43:27 -0500

    added logout logic

---
+++ app/logout.php
@@ -0,0 +1,14 @@
+<?php
+
+session_start();
+require 'functions.php';
+
+if( $_SESSION['is_logged_in'] ){
+	session_destroy();
+	if( !redirect_signin() ) die('did not redirect to sign in from logout');
+}
+else{
+	die('Should not be logged in');
+}
+
+?>
