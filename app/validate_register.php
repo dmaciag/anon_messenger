d191fb6563bf6c2d2e87e7a2eeb68c60a80a53bb
Daniel Maciag <daniel@vettery.com>
2016-01-06 22:55:26 -0500

    whitespace removal

--- app/validate_register.php
+++ app/validate_register.php
@@ -10,9 +10,9 @@ if( !mysql_select_db($db_db) ) die('Cannot connect to db : $db_db, ' . mysql_err
 
 function strip_data( $data_to_be_stripped ){
 
-	$data_to_be_stripped = 						trim($data_to_be_stripped);
-	$data_to_be_stripped = 				stripslashes($data_to_be_stripped);
-	$data_to_be_stripped = 			htmlspecialchars($data_to_be_stripped);
+	$data_to_be_stripped = 	trim($data_to_be_stripped);
+	$data_to_be_stripped = 	stripslashes($data_to_be_stripped);
+	$data_to_be_stripped = 	htmlspecialchars($data_to_be_stripped);
 	$data_to_be_stripped = 	mysql_real_escape_string($data_to_be_stripped);
 
 	return $data_to_be_stripped;
