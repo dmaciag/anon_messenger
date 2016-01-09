70494f7569aea319bab3863c53c99bdf46972433
Daniel Maciag <daniel@vettery.com>
2016-01-09 01:54:27 -0500

    angular ajax response fully working in backend

--- app/friend_search.php
+++ app/friend_search.php
@@ -1,13 +1,6 @@
-<?php
+<?php 
 
-session_start();
-
-require '../functions.php';
 require '../config.php';
-
-if( !$_SESSION['is_logged_in'] ){
-	if( !redirect_signin() ) die( 'failed to redirect to signin from friend search ajax' ); 
-}
 
 $connect = mysql_connect($db_hostname, $db_username, $db_password);
 
@@ -15,19 +8,19 @@ if( !$connect ) die('Connection to mysql failed, error : ' . mysql_error());
 
 if( !mysql_select_db($db_db) ) die('Cannot connect to db : $db_db, ' . mysql_error());
 
-$friend_input = $_GET['friend_input'];
-
-for($i = 0; $i < strlen($friend_input); $i++){
-	$regexp_user .= "[" . $friend_input[$i] . "]"; 
-}
-
-$registered_users_sql = "SELECT username FROM registered_users WHERE username REGEXP '^$regexp_user.*$'";
+$registered_users_sql = "SELECT * FROM registered_users";
 $registered_users = mysql_query( $registered_users_sql );
 
 while( $user = mysql_fetch_assoc($registered_users) ){
-	echo $user["username"] . "<br>";
+    $json_user['username'] = $user["username"];
+    $json_user_response[] = $json_user;
+
 }
+$search_arr = array("users" => $json_user_response);
 
 mysql_close();
+
+echo json_encode($search_arr);
+
 
 ?>
