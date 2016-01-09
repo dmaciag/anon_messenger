608d335780d2c36a1fad6c47c54adf3014242303
Daniel Maciag <daniel@vettery.com>
2016-01-09 01:45:36 -0500

    added partial user lookup search functionality

--- js/messenger.js
+++ js/messenger.js
@@ -1,6 +1,25 @@
-var messenger_app = angular.module('messenger', []);
+var app = angular.module('myApp', []);
 
-messenger_app.controller('usersCtrl', function($scope, $http) {
-    $http.get("http://www.w3schools.com/angular/customers.php")
-    .then(function(response) {$scope.names = response.data.records;});
+app.controller('customersCtrl', function($scope, $http) {
+    $http.get("./testing.php")
+    .then(function(response) {
+        $scope.names = response.data.users;
+      });
+});
+
+app.filter('user_search_filter', function(){
+	return function(search_array, search_query){
+		if(!search_query){
+			return search_array;
+		}
+
+		var search_result = [];
+		angular.forEach(search_array, function(user){
+			if(user.username.indexOf(search_query) !== -1){
+				search_result.push(user);
+			}
+		});
+
+		return search_result;
+	}
 });
